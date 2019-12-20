be7050127e17fd67f3c82525144c732479b83e89
Denilson Leite <denilson.dlr@gmail.com>
2019-12-20 16:48:57 -0300

    java11 bdd

--- src/main/java/br/com/deni/hexagonal/HexagonalApplication.java
+++ src/main/java/br/com/deni/hexagonal/HexagonalApplication.java
@@ -3,12 +3,14 @@ package br.com.deni.hexagonal;
 
 import br.com.deni.hexagonal.adapters.ReceiverAdapter;
 import br.com.deni.hexagonal.adapters.SenderAdapter;
+import br.com.deni.hexagonal.core.domain.User;
 import br.com.deni.hexagonal.core.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
 
 
 @SpringBootApplication
@@ -30,14 +32,14 @@ public class HexagonalApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        //sender.send("Hello Spring JMS ActiveMQ!");
-        //User john = new User(null, "John Yellow");
-        //userRepository.save(john);
-        //User maria = new User(null, "Maria Brown");
-       // User alex = new User(null, "Alex Green");
-        //User bob = new User(null, "Bob Grey");
+        sender.send("Hello Spring JMS ActiveMQ!");
+        User john = new User(null, "John Yellow");
+        userRepository.save(john);
+        User maria = new User(null, "Maria Brown");
+       User alex = new User(null, "Alex Green");
+       User bob = new User(null, "Bob Grey");
 
-        //userRepository.saveAll(Arrays.asList(maria, alex, bob));
+       userRepository.saveAll(Arrays.asList(maria, alex, bob));
 
 
     }
